a7d90c11d234b2a3de2ebd1a18562e1e6d8926af
Mike Childs <mike@mjcdev.co.uk>
2019-12-05 20:47:01 +0000

    Day 05 Part One

---
+++ AOC2019.Days.Tests/Day05Tests.cs
@@ -0,0 +1,60 @@
+﻿using AOC2019.Days.D05;
+using AOC2019.Days.Tests.Abstract;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AOC2019.Days.Tests
+{
+    public class Day05Tests : AbstractTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public Day05Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+               
+        [Fact]
+        public void DayFivePartOne()
+        {
+            var input = ReadCommaSeperatedLine("Day05.txt", i => int.Parse(i)).ToArray();
+
+            var output = new Day05().Execute(input);
+        }
+
+        [Fact]
+        public void DayTwoPartTwo()
+        {
+            var anticipatedResult = 19690720;
+
+            for (var noun = 0; noun <= 99; noun++)
+            {
+                for (var verb = 0; verb <= 99; verb++)
+                {
+                    var input = ReadCommaSeperatedLine<int>("Day02.txt", i => int.Parse(i)).ToArray();
+
+                    input[1] = noun;
+                    input[2] = verb;
+
+                    var output = new Day05().Execute(input);
+
+                    var result = output[0];
+
+                    if (result == anticipatedResult)
+                    {
+                        var answer = (100 * noun) + verb;
+
+                        _output.WriteLine(answer.ToString());
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AOC2019.Days/D05/Day05.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2019.Days.D05
+{
+    public class Day05
+    {
+        private const int Finish = 99;
+        private const int Addition = 1;
+        private const int Multiply = 2;
+        private const int Input = 3;
+        private const int Output = 4;
+
+        private const int ParameterLengthFour = 4;
+        private const int ParameterLengthTwo = 2;
+
+        private const int PositionMode = 0;
+        private const int ImmediateMode = 1;
+
+        public int[] Execute(int[] input)
+        {
+            var index = 0;         
+            
+            while (true)
+            {
+                var instruction = input[index];
+
+                var opcode = instruction % 100;
+                instruction /= 100;
+                var firstParameterMode = instruction % 10;
+                instruction /= 10;
+                var secondParameterMode = instruction % 10;
+                instruction /= 10;
+                var thirdParameterMode = instruction % 10;
+
+                var outputs = new List<int>();
+
+                if (opcode == Addition)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+                    var second = GetParameter(input, secondParameterMode, index + 2);
+                    var third = input[index + 3];
+
+                    input[third] = first + second;
+                    index += ParameterLengthFour;
+                }
+                else if (opcode == Multiply)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+                    var second = GetParameter(input, secondParameterMode, index + 2);
+                    var third = input[index + 3];
+
+                    input[third] = first * second;
+                    index += ParameterLengthFour;
+                }
+                else if (opcode == Input)
+                {
+                    input[input[index + 1]] = 1;
+                    index += ParameterLengthTwo;
+                }
+                else if (opcode == Output)
+                {
+                    var outputValue = GetParameter(input, firstParameterMode, index + 1);
+
+                    outputs.Add(outputValue);
+
+                    if (outputValue != 0)
+                    {
+                        throw new Exception();
+                    }
+                    
+                    index += ParameterLengthTwo;
+                }
+                else if (opcode == Finish)
+                {
+                    return input;
+                }
+            }
+        }
+
+        private int GetParameter(int[] input, int parameterMode, int parameterIndex)
+        {
+            if (parameterMode == PositionMode)
+            {
+                return input[input[parameterIndex]];
+            }
+            else if (parameterMode == ImmediateMode)
+            {
+                return input[parameterIndex];
+            }
+
+            throw new Exception();
+        }
+    }
+}
