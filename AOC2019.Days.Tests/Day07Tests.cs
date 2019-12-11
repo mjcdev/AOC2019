ca6bed96c791eb15effe72fe34dd3457e526428a
Mike Childs <mjcdev@users.noreply.github.com>
2019-12-11 20:47:36 +0000

    Day 7 Part One

---
+++ AOC2019.Days.Tests/Day07Tests.cs
@@ -0,0 +1,59 @@
+﻿using AOC2019.Days.D07;
+using AOC2019.Days.Tests.Abstract;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+
+namespace AOC2019.Days.Tests
+{
+    public class Day07Tests : AbstractTests
+    {
+        [Fact]
+        public void GetCombinations()
+        {
+            var results = new Day07().GetCombinations(new[] { 1, 2, });
+
+            results.Should().HaveCount(2);
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0 }, new[] { 4, 3, 2, 1, 0 }, 43210)]
+        [InlineData(new [] {  3,23,3,24,1002,24,10,24,1002,23,-1,23, 101,5,23,23,1,24,23,23,4,23,99,0,0}, new[] { 0, 1, 2, 3, 4 }, 54321)]
+        [InlineData(new [] {  3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0}, new[] { 1, 0, 4, 3, 2 }, 65210)]
+        public void CalculateOutput(int[] inputArray, int[] combination, int expected)
+        {
+            new Day07().CalculateOutput(combination, inputArray, 0).Should().Be(expected);
+        }
+
+        [Fact]
+        public void DaySevenPartOne()
+        {
+            var day = new Day07();
+
+            var combinations = day.GetCombinations(new[] { 0, 1, 2, 3, 4 });
+
+            var input = ReadCommaSeperatedLine("Day07.txt", i => int.Parse(i)).ToArray();
+
+            var maximum = combinations.Max(c => day.CalculateOutput(c, input, 0));
+        }
+
+        [Theory]
+        [InlineData(new[] { 3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26, 27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5 }, new[] { 9, 8, 7, 6, 5, }, 139629729)]
+        public void CalculateOutputUntilHalted(int[] inputArray, int[] combination, int expected)
+        {
+            new Day07().CalculateOutputUntilHalted(combination, inputArray, 0).Should().Be(expected);
+        }
+
+        [Fact]
+        public void DaySeventPartTwo()
+        {
+            var day = new Day07();
+
+            var combinations = day.GetCombinations(new[] { 5, 6, 7, 8, 9, });
+
+            var input = ReadCommaSeperatedLine("Day07.txt", i => int.Parse(i)).ToArray();
+
+            var maximum = combinations.Max(c => day.CalculateOutputUntilHalted(c, input, 0));
+        }
+    }
+}
---
+++ AOC2019.Days/D07/Day07.cs
@@ -0,0 +1,209 @@
+﻿using Combinatorics.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOC2019.Days.D07
+{
+    public class Day07
+    {
+        private const int Finish = 99;
+        private const int Addition = 1;
+        private const int Multiply = 2;
+        private const int Input = 3;
+        private const int Output = 4;
+        private const int JumpIfTrue = 5;
+        private const int JumpIfFalse = 6;
+        private const int LessThan = 7;
+        private const int Equals = 8;
+
+        private const int ParameterLengthFour = 4;
+        private const int ParameterLengthThree = 3;
+        private const int ParameterLengthTwo = 2;
+
+        private const int PositionMode = 0;
+        private const int ImmediateMode = 1;
+
+        public (bool halt, int[] result) Execute(int[] input, int userInput, int thrusterOutput)
+        {
+            var index = 0;
+
+            var useUserInput = true;
+
+            var outputs = new List<int>();
+
+            while (true)
+            {
+                var instruction = input[index];
+
+                var opcode = instruction % 100;
+                instruction /= 100;
+                var firstParameterMode = instruction % 10;
+                instruction /= 10;
+                var secondParameterMode = instruction % 10;
+
+                if (opcode == Addition)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+                    var second = GetParameter(input, secondParameterMode, index + 2);
+                    var third = input[index + 3];
+
+                    input[third] = first + second;
+                    index += ParameterLengthFour;
+                }
+                else if (opcode == Multiply)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+                    var second = GetParameter(input, secondParameterMode, index + 2);
+                    var third = input[index + 3];
+
+                    input[third] = first * second;
+                    index += ParameterLengthFour;
+                }
+                else if (opcode == Input)
+                {
+                    var inputValue = 0;
+                    
+                    if (useUserInput)
+                    {
+                        inputValue = userInput;
+                        useUserInput = false;
+                    }
+                    else
+                    {
+                        inputValue = thrusterOutput;
+                    }
+                    
+                    input[input[index + 1]] =  inputValue;
+                    index += ParameterLengthTwo;
+                }
+                else if (opcode == Output)
+                {
+                    var outputValue = GetParameter(input, firstParameterMode, index + 1);
+
+                    outputs.Add(outputValue);
+
+                    if (outputValue != 0)
+                    {
+                        return (false, outputs.ToArray());
+                     //   throw new Exception();
+                    }
+
+                    index += ParameterLengthTwo;
+                }
+                else if (opcode == JumpIfTrue)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+
+                    if (first != 0)
+                    {
+                        var second = GetParameter(input, secondParameterMode, index + 2);
+                        index = second;
+                    }
+                    else
+                    {
+                        index += ParameterLengthThree;
+                    }
+                }
+                else if (opcode == JumpIfFalse)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+
+                    if (first == 0)
+                    {
+                        var second = GetParameter(input, secondParameterMode, index + 2);
+                        index = second;
+                    }
+                    else
+                    {
+                        index += ParameterLengthThree;
+                    }
+                }
+                else if (opcode == LessThan)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+                    var second = GetParameter(input, secondParameterMode, index + 2);
+                    var third = input[index + 3];
+
+                    input[third] = first < second ? 1 : 0;
+
+                    index += ParameterLengthFour;
+                }
+                else if (opcode == Equals)
+                {
+                    var first = GetParameter(input, firstParameterMode, index + 1);
+                    var second = GetParameter(input, secondParameterMode, index + 2);
+                    var third = input[index + 3];
+
+                    input[third] = first == second ? 1 : 0;
+
+                    index += ParameterLengthFour;
+                }
+                else if (opcode == Finish)
+                {
+                    return (true, outputs.ToArray());
+                }
+            }
+        }
+
+        private int GetParameter(int[] input, int parameterMode, int parameterIndex)
+        {
+            if (parameterMode == PositionMode)
+            {
+                return input[input[parameterIndex]];
+            }
+            else if (parameterMode == ImmediateMode)
+            {
+                return input[parameterIndex];
+            }
+
+            throw new Exception();
+        }
+
+        public IEnumerable<IList<int>> GetCombinations(IEnumerable<int> inputs)
+        {
+            var permutations = new Permutations<int>(inputs.ToArray());
+
+            return permutations;
+        }
+
+        public int CalculateOutput(IList<int> combination, int[] input, int thrusterInput)
+        {            
+            IEnumerable<int> result = Array.Empty<int>();
+            
+            foreach (var value in combination)
+            {
+                var copy = input.ToArray();
+                result = Execute(copy, value, thrusterInput).result;
+                thrusterInput = result.First();
+            }
+
+            return result.First();
+        }
+
+        public int CalculateOutputUntilHalted(IList<int> combination, int[] input, int thrusterInput)
+        {
+            IEnumerable<int> result = Array.Empty<int>();
+
+            var halted = false;
+
+            while (!halted)
+            {
+                foreach (var value in combination)
+                {
+                    var copy = input.ToArray();
+                    var executionResult = Execute(copy, value, thrusterInput);
+                    result = executionResult.result;
+                    thrusterInput = result.First();
+                    if (executionResult.halt)
+                    {
+                        halted = executionResult.halt;
+                    }
+                }
+            }
+
+            return result.First();
+        }
+    }
+}
